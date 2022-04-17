821264450c2bfe6ae03e062d768642083c324dec
Sam <earthstar412@gmail.com>
2022-04-17 06:02:06 +0000

    edits

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,18 +11,18 @@ void bubble_sort(int *array, size_t size)
 	size_t x, y;
 	int z;
 
-	if (array != NULL || size < 2)
+	if (array == NULL || size < 2)
 		return;
 
-	for (x = 0; x < (size - 1); x++)
+	for (x = 0; x < size - 1; x++)
 	{
-		for (y = 0; y < (size - x - 1); y++)
+		for (y = 0; y < size - 1; y++)
 		{
 		if (array[y] > array[y + 1])
 		{
-			z = array[y];
+			z = array[y + 1];
 			array[y] = array[y + 1];
-			array[y + 1] = z;
+			array[y] = z;
 			print_array(array, size);
 		}
 		}
