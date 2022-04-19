445c9ed0867f5ac36609d63198a59ecad3a7d3d1
bashthat <3249@holbertonschool.com>
2022-04-19 02:03:55 -0700

    betty

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,30 +1,30 @@
 #include "sort.h"
 
 /**
- * bubble_sort - a function to perform a bubble sort ordering system with an algorithm
+ * bubble_sort - a function algorithm
  * @array: the array.
  * @size: array size.
  * Retrun: null
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t x, y;
-	int z;
+size_t x, y;
+int z;
 
-	if (array == NULL || size < 2)
-		return;
+if (array == NULL || size < 2)
+return;
 
-	for (x = 0; x < size - 1; x++)
-	{
-		for (y = 0; y < size - 1; y++)
-		{
-		if (array[y] > array[y + 1])
-		{
-			z = array[y + 1];
-			array[y] = array[y + 1];
-			array[y] = z;
-			print_array(array, size);
-		}
-		}
-	}
+for (x = 0; x < size - 1; x++)
+{
+for (y = 0; y < size - 1; y++)
+{
+if (array[y] > array[y + 1])
+{
+z = array[y + 1];
+array[y] = array[y + 1];
+array[y] = z;
+print_array(array, size);
+}
+}
+}
 }
